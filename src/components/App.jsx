7a1b3c5ac5eb7335b8d1961d5b00230b3d8bc54e
DanielaBadea <badeadaniella@gmail.com>
2024-05-01 04:59:10 +0300

    add atribute end at route

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -28,9 +28,9 @@ export const App = () => {
       <Suspense fallback={<Spinner/>}>
       <Routes>
         {/* <Route path="/" element = {<NavigationBar/>} /> */}
-        <Route path="/" element={<Home/>}/>
-        <Route path="movies" element = {<Movies/>} />
-        <Route path="movies/:movieId" element = {<MoviesDetails/>}>
+        <Route path="/" end element={<Home/>}/>
+        <Route path="/movies" element = {<Movies/>} />
+        <Route path="/movies/:movieId" element = {<MoviesDetails/>}>
         <Route path="cast" element={<Cast />} />
         <Route path="reviews" element={<Reviews/>}/>
         </Route>
